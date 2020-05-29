af0be60c52e2c713be8fe48ebf79b36a8a95c24a
unknown <rahulmaurya032@gmail.com>
2020-05-29 22:12:14 +0530

    Add item and calling in UI

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
+// TODO : BUDGET CONTROLLER MODULE
 var budgetcontroller = (function () {
+    var expenses = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var data = {
+        additems: {
+            exp: [],
+            inc: []
+        },
+        total: {
+            ex: 0,
+            inc: 0
+        }
+    };
+    return {
+        addItem: function (type, des, val) {
+            var newitems, ID;
+            if (data.additems[type].length > 0) {
+                ID = data.additems[type][data.additems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newitems = new expenses(ID, des, val);
+            } else if (type === 'inc') {
+                newitems = new income(ID, des, val)
+            }
+            data.additems[type].push(newitems);
+            return newitems;
+        },
+        testing: function () {
+            console.log(data);
+        }
+    }
 
 })();
-
+// TODO : UI CONTROLLER MODULE
 var UIcontroller = (function () {
     var DomString = {
         inputType: '.add__type',
@@ -13,7 +55,7 @@ var UIcontroller = (function () {
     return {
         getinput: function () {
             return {
-                type: document.querySelector(DomString.inputType).value,
+                id: document.querySelector(DomString.inputType).value,
                 description: document.querySelector(DomString.inputDes).value,
                 valuee: document.querySelector(DomString.inputValue).value
             };
@@ -21,11 +63,9 @@ var UIcontroller = (function () {
         getDomString: function () {
             return DomString;
         }
-
     };
-
 })();
-
+//  TODO : TOTAL CONTROLLER MODULE
 var controller = (function (budgetctrl, uictrl) {
     var callEventListner = function () {
         var Dom = uictrl.getDomString();
@@ -40,6 +80,7 @@ var controller = (function (budgetctrl, uictrl) {
         // 1. get the field input data 
         // 2.add the to the budget controller 
         var result = uictrl.getinput();
+        var newItem = budgetctrl.addItem(result.type, result.description, result.value)
 
         // 3. add the item to the UI
 
@@ -49,7 +90,7 @@ var controller = (function (budgetctrl, uictrl) {
     }
     return {
         init: function () {
-            console.log('the application is started')
+            console.log('the application is started');
             callEventListner();
         }
     }
