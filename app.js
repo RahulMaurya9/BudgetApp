fe7848abd2a39772c815b1e1a6c8f8515e7c84d2
unknown <rahulmaurya032@gmail.com>
2020-05-29 15:30:10 +0530

    some changes

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ var budgetcontroller = (function () {
 })();
 
 var UIcontroller = (function () {
+    var DomString = {
+        inputType: '.add__type',
+        inputDes: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getinput: function () {
+            return {
+                type: document.querySelector(DomString.inputType).value,
+                description: document.querySelector(DomString.inputDes).value,
+                valuee: document.querySelector(DomString.inputValue).value
+            };
+        },
+        getDomString: function () {
+            return DomString;
+        }
+
+    };
 
 })();
 
 var controller = (function (budgetctrl, uictrl) {
+    var callEventListner = function () {
+        var Dom = uictrl.getDomString();
+        document.querySelector(Dom.inputBtn).addEventListener('click', ctrladdItem);
+        document.querySelector('keypres', function (event) {
+            if (event.keycode === 13 || event.which === 13)
+                ctrladdItem();
+        });
+    }
+
     function ctrladdItem() {
         // 1. get the field input data 
-
         // 2.add the to the budget controller 
-        console.log('you click the enter button')
+        var result = uictrl.getinput();
 
         // 3. add the item to the UI
 
@@ -19,10 +47,12 @@ var controller = (function (budgetctrl, uictrl) {
 
         // 5. Display the  budget on the ui
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrladdItem);
-    document.querySelector('keypres', function (event) {
-        if (event.keycode === 13 || event.which === 13)
-            ctrladdItem();
-    });
+    return {
+        init: function () {
+            console.log('the application is started')
+            callEventListner();
+        }
+    }
 
 })(budgetcontroller, UIcontroller);
+controller.init();
